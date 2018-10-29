de81f294a91ae8a015687812c5e2668c8751728d
Song Zhang <yitengruntu12123@gmail.com>
2018-10-29 15:17:21 +0800

    init

---
+++ examples/starter-bot.js
@@ -0,0 +1,35 @@
+const { Wechaty } = require('wechaty')
+
+const bot = new Wechaty()
+
+bot.on('scan',    onScan)
+bot.on('login',   onLogin)
+bot.on('logout',  onLogout)
+bot.on('message', onMessage)
+
+bot.start()
+.then(() => console.log('Starter Bot Started.'))
+.catch(e => console.error(e))
+
+function onScan (qrcode, status) {
+  require('qrcode-terminal').generate(qrcode, { small: true })  // show qrcode on console
+
+  const qrcodeImageUrl = [
+    'https://api.qrserver.com/v1/create-qr-code/?data=',
+    encodeURIComponent(qrcode),
+  ].join('')
+
+  console.log(qrcodeImageUrl)
+}
+
+function onLogin (user) {
+  console.log(`${user} login`)
+}
+
+function onLogout(user) {
+  console.log(`${user} logout`)
+}
+
+async function onMessage (msg) {
+  console.log(msg.toString())
+}
